ea3f57e41def80c7688036cb4b9eb9a10c61e5b2
Akef Habbal <m.akef.test@gmail.com>
2024-06-06 15:54:17 +0300

    finished static table generation

--- README.md
+++ README.md
@@ -25,3 +25,4 @@
   https://www.youtube.com/playlist?list=PL1w1q3fL4pmj9k1FrJ3Pe91EPub2_h4jF
   https://code.visualstudio.com/docs/copilot/overview
   https://medium.com/@rijuldahiya/exporting-json-data-to-pdf-in-angular-using-pdfmake-aef64dbd1606
+  https://github.com/bpampuch/pdfmake/issues/2018
--- src/app/po-generator/po-generator.component.ts
+++ src/app/po-generator/po-generator.component.ts
@@ -6,7 +6,13 @@ import { HousingLocation } from "../housing-location";
 import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 import * as pdfFonts from "pdfmake/build/vfs_fonts";
 import * as pdfMake from "pdfmake/build/pdfmake";
-import { TDocumentDefinitions } from "pdfmake/interfaces";
+// import * as htmlToPdfmake from "html-to-pdfmake";
+var htmlToPdfmake = require("html-to-pdfmake");
+import {
+  Style,
+  StyleDictionary,
+  TDocumentDefinitions,
+} from "pdfmake/interfaces";
 
 // pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
@@ -17,15 +23,40 @@ import { TDocumentDefinitions } from "pdfmake/interfaces";
   template: `
     <article class="details">
       <section class="listing-features">
-        <h2 class="section-heading">About his housing location</h2>
-        <ul>
-          <li>Units available: {{ housingLocation?.availableUnits }}</li>
-          <li>Wi-Fi</li>
-          <li>Laundry</li>
-        </ul>
+        <div>
+          <header>
+            <h1>Purchase Order #1001</h1>
+            <p>Created by: Ahmad Hussein</p>
+            <p>Issued at: 10/05/2024</p>
+          </header>
+          <table class="styled-table">
+            <thead>
+              <tr>
+                <th>Select</th>
+                <th>Name</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><input type="checkbox" /></td>
+                <td>Concrete</td>
+                <td>6000 QR</td>
+              </tr>
+              <tr class="active-row">
+                <td><input type="checkbox" /></td>
+                <td>Sand</td>
+                <td>5150 QR</td>
+              </tr>
+              <!-- and so on... -->
+            </tbody>
+          </table>
+        </div>
       </section>
 
-      <button (click)="generatePdf()">Download PDF</button>
+      <button class="primary btn" type="button" (click)="generatePdf()">
+        Generate PDF
+      </button>
     </article>
   `,
   styleUrls: ["./po-generator.component.css"],
@@ -70,13 +101,111 @@ export class POGeneratorComponent {
         { text: "User Data", style: "header" },
         { table: { body: data } },
       ],
-      styles: {
-        header: { fontSize: 18, bold: true, margin: [0, 0, 0, 10] },
+      // styles: {
+      //   header: { fontSize: 18, bold: true, margin: [0, 0, 0, 10] },
+      // },
+      footer: function (currentPage, pageCount) {
+        return currentPage.toString() + " of " + pageCount;
+      },
+      header: function (currentPage, pageCount, pageSize) {
+        // you can apply any logic and return any valid pdfmake element
+
+        return [
+          {
+            text: "simple text",
+            alignment: currentPage % 2 ? "left" : "right",
+          },
+          {
+            canvas: [
+              { type: "rect", x: 170, y: 32, w: pageSize.width - 170, h: 40 },
+            ],
+          },
+        ];
+      },
+    };
+
+    var html = htmlToPdfmake(
+      `
+      <div>
+        <header>
+          <h1>Purchase Order #1001</h1>
+          <p>Created by: Ahmad Hussein</p>
+          <p>Issued at: 10/05/2024</p>
+        </header>
+        <table class="fTable">
+          <thead class="tThead">
+              <tr class="trs">
+                  <th class="thds">Name</th>
+                  <th class="thds">Price</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr class="trs">
+                  <td class="thds">Concrete</td>
+                  <td class="thds">6000 QR</td>
+              </tr>
+              <tr class="active-row">
+                  <td class="thds">Sand</td>
+                  <td class="thds">5150 QR</td>
+              </tr>
+          </tbody>
+        </table>
+      </div>
+    `
+    );
+
+    const styles: StyleDictionary = {
+      tThead: {
+        // background: "#009879",
+        // color: "#ffffff",
+        alignment: "left",
+      },
+      title: {
+        color: "red",
+      },
+      header: {
+        fontSize: 18,
+        bold: true,
+        margin: [0, 0, 0, 10],
+      },
+      thds: {
+        columnGap: 10,
+        margin: 20,
+      },
+    };
+    var dd: TDocumentDefinitions = {
+      content: [
+        html,
+        {
+          absolutePosition: { x: 40, y: 650 },
+          table: {
+            widths: ["*"],
+            heights: [100],
+            body: [
+              [
+                {
+                  text: "Signature               ____________________________",
+                  margin: [0, 50, 0, 0],
+                  border: [true, true, true, true],
+                },
+              ],
+            ],
+          },
+        },
+      ],
+      styles,
+      header: {
+        text: "Al Attiyah Company",
+        alignment: "center",
+        margin: [0, 10, 0, 0],
+      },
+      footer: function (currentPage, pageCount) {
+        return "Page: " + currentPage.toString() + " of " + pageCount;
       },
     };
 
     pdfMake
-      .createPdf(docDefinition, {}, pdfMake.fonts, pdfFonts.pdfMake.vfs)
+      .createPdf(dd, {}, pdfMake.fonts, pdfFonts.pdfMake.vfs)
       .download("Purchase Order.pdf");
   }
 }
